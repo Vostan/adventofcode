4514cd7aa47d7a39fda74bc4af5fa55f80e29fbd
vostan <vostan.92@gmail.com>
2017-09-20 10:46:12 +0300

    generalized

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -21,78 +21,33 @@ class Day1 {
 
             if((position.direction == "N" && turn == "R")
                     || (position.direction == "S" && turn == "L")){
-                if(moveEast(position, steps) == null) return position;
+                if(move(position, "E", steps) == null) return position;
             } else if ((position.direction == "N" && turn == "L")
                     || (position.direction == "S" && turn == "R")){
-                if(moveWest(position, steps) == null) return position;
+                if(move(position, "W", steps) == null) return position;
             } else if ((position.direction == "W" && turn == "R")
                     || (position.direction == "E" && turn == "L")){
-                if(moveNorth(position, steps) == null) return position;
+                if(move(position, "N", steps) == null) return position;
             } else {
-                if(moveSouth(position, steps) == null) return position;
+                if(move(position, "S", steps) == null) return position;
             }
 
             return doMove(position, cords.drop(1));
 
         }
 
-        private fun moveEast(position: Position, steps: Int): Position? {
+        private fun move(position: Position, direction: String, steps: Int): Position?{
 
-            position.direction = "E";
+            position.direction = direction;
             for (step in 1..steps){
 
-                position.x += 1;
-                if(Position.contains(Pair(position.x, position.y))){
-                    return null;
-                } else {
-                    Position.vistis.add(Pair(position.x, position.y));
+                when(direction){
+                    "S" -> position.y -= 1;
+                    "N" -> position.y += 1;
+                    "W" -> position.x -= 1;
+                    "E" -> position.x += 1;
                 }
 
-            }
-
-            return position;
-        }
-
-        private fun moveWest(position: Position, steps: Int): Position? {
-
-            position.direction = "W";
-            for (step in 1..steps){
-
-                position.x -= 1;
-                if(Position.contains(Pair(position.x, position.y))){
-                    return null;
-                } else {
-                    Position.vistis.add(Pair(position.x, position.y));
-                }
-
-            }
-
-            return position;
-        }
-
-        private fun moveNorth(position: Position, steps: Int): Position? {
-
-            position.direction = "N";
-            for (step in 1..steps){
-
-                position.y += 1;
-                if(Position.contains(Pair(position.x, position.y))){
-                    return null;
-                } else {
-                    Position.vistis.add(Pair(position.x, position.y));
-                }
-                
-            }
-
-            return position;
-        }
-
-        private fun moveSouth(position: Position, steps: Int): Position? {
-
-            position.direction = "S";
-            for (step in 1..steps){
-
-                position.y -= 1;
                 if(Position.contains(Pair(position.x, position.y))){
                     return null;
                 } else {
