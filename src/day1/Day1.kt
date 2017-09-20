e72731dc72e2ae8511f00b01f77b78a03d0ca3fe
vostan <vostan.92@gmail.com>
2017-09-20 10:35:59 +0300

    second day done, some modifications in the first day need to optimize

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -1,73 +1,115 @@
 package day1
 
+import javafx.geometry.Pos
+
 class Day1 {
 
-    fun main(args: Array<String>) {
-        print(doMove(Position("N", 0, 0), getCordinates()).getDistance());
-    }
+    companion object {
 
-    private fun doMove(position: Position, cords: List<String>): Position {
+        @JvmStatic fun main(args: Array<String>) {
+            print(doMove(Position("N", 0, 0), getCordinates()).getDistance());
+        }
 
-        if(cords.isEmpty()){
+        private fun doMove(position: Position, cords: List<String>): Position {
+
+            if(cords.isEmpty()){
+                return position;
+            }
+
+            val turn = cords[0].replace(Regex("[0-9]"), "");
+            val steps = cords[0].replace(Regex("[^0-9]"), "").toInt();
+
+            if((position.direction == "N" && turn == "R")
+                    || (position.direction == "S" && turn == "L")){
+                if(moveEast(position, steps) == null) return position;
+            } else if ((position.direction == "N" && turn == "L")
+                    || (position.direction == "S" && turn == "R")){
+                if(moveWest(position, steps) == null) return position;
+            } else if ((position.direction == "W" && turn == "R")
+                    || (position.direction == "E" && turn == "L")){
+                if(moveNorth(position, steps) == null) return position;
+            } else {
+                if(moveSouth(position, steps) == null) return position;
+            }
+
+            return doMove(position, cords.drop(1));
+
+        }
+
+        private fun moveEast(position: Position, steps: Int): Position? {
+
+            position.direction = "E";
+            for (step in 1..steps){
+
+                position.x += 1;
+                if(Position.contains(Pair(position.x, position.y))){
+                    return null;
+                } else {
+                    Position.vistis.add(Pair(position.x, position.y));
+                }
+
+            }
+
             return position;
         }
 
-        val turn = cords[0].replace(Regex("[0-9]"), "");
-        val steps = cords[0].replace(Regex("[^0-9]"), "").toInt();
+        private fun moveWest(position: Position, steps: Int): Position? {
 
-        if((position.direction == "N" && turn == "R")
-                || (position.direction == "S" && turn == "L")){
-            moveEast(position, steps);
-        } else if ((position.direction == "N" && turn == "L")
-                || (position.direction == "S" && turn == "R")){
-            moveWest(position, steps);
-        } else if ((position.direction == "W" && turn == "R")
-                || (position.direction == "E" && turn == "L")){
-            moveNorth(position, steps);
-        } else {
-            moveSouth(position, steps);
+            position.direction = "W";
+            for (step in 1..steps){
+
+                position.x -= 1;
+                if(Position.contains(Pair(position.x, position.y))){
+                    return null;
+                } else {
+                    Position.vistis.add(Pair(position.x, position.y));
+                }
+
+            }
+
+            return position;
         }
 
-        return doMove(position, cords.drop(1));
+        private fun moveNorth(position: Position, steps: Int): Position? {
 
-    }
+            position.direction = "N";
+            for (step in 1..steps){
 
-    private fun moveEast(position: Position, steps: Int): Position {
+                position.y += 1;
+                if(Position.contains(Pair(position.x, position.y))){
+                    return null;
+                } else {
+                    Position.vistis.add(Pair(position.x, position.y));
+                }
+                
+            }
 
-        position.direction = "E";
-        position.x += steps;
+            return position;
+        }
 
-        return position;
-    }
+        private fun moveSouth(position: Position, steps: Int): Position? {
 
-    private fun moveWest(position: Position, steps: Int): Position {
+            position.direction = "S";
+            for (step in 1..steps){
 
-        position.direction = "W";
-        position.x -= steps;
+                position.y -= 1;
+                if(Position.contains(Pair(position.x, position.y))){
+                    return null;
+                } else {
+                    Position.vistis.add(Pair(position.x, position.y));
+                }
 
-        return position;
-    }
+            }
 
-    private fun moveNorth(position: Position, steps: Int): Position {
+            return position;
+        }
 
-        position.direction = "N";
-        position.y += steps;
+        private fun getCordinates(): List<String> {
+            val input = "L4, R2, R4, L5, L3, L1, R4, R5, R1, R3, L3, L2, L2, R5, R1, L1, L2, R2, R2, L5, R5, R5, L2, R1, R2, L2, L4, L1, R5, R2, R1, R1, L2, L3, R2, L5, L186, L5, L3, R3, L5, R4, R2, L5, R1, R4, L1, L3, R3, R1, L1, R4, R2, L1, L4, R5, L1, R50, L4, R3, R78, R4, R2, L4, R3, L4, R4, L1, R5, L4, R1, L2, R3, L2, R5, R5, L4, L1, L2, R185, L5, R2, R1, L3, R4, L5, R2, R4, L3, R4, L2, L5, R1, R2, L2, L1, L2, R2, L2, R1, L5, L3, L4, L3, L4, L2, L5, L5, R2, L3, L4, R4, R4, R5, L4, L2, R4, L5, R3, R1, L1, R3, L2, R2, R1, R5, L4, R5, L3, R2, R3, R1, R4, L4, R1, R3, L5, L1, L3, R2, R1, R4, L4, R3, L3, R3, R2, L3, L3, R4, L2, R4, L3, L4, R5, R1, L1, R5, R3, R1, R3, R4, L1, R4, R3, R1, L5, L5, L4, R4, R3, L2, R1, R5, L3, R4, R5, L4, L5, R2";
 
-        return position;
-    }
+            return input.split(", ");
+        }
 
-    private fun moveSouth(position: Position, steps: Int): Position {
-
-        position.direction = "S";
-        position.y -= steps;
-
-        return position;
-    }
-
-    private fun getCordinates(): List<String> {
-        val input = "L4, R2, R4, L5, L3, L1, R4, R5, R1, R3, L3, L2, L2, R5, R1, L1, L2, R2, R2, L5, R5, R5, L2, R1, R2, L2, L4, L1, R5, R2, R1, R1, L2, L3, R2, L5, L186, L5, L3, R3, L5, R4, R2, L5, R1, R4, L1, L3, R3, R1, L1, R4, R2, L1, L4, R5, L1, R50, L4, R3, R78, R4, R2, L4, R3, L4, R4, L1, R5, L4, R1, L2, R3, L2, R5, R5, L4, L1, L2, R185, L5, R2, R1, L3, R4, L5, R2, R4, L3, R4, L2, L5, R1, R2, L2, L1, L2, R2, L2, R1, L5, L3, L4, L3, L4, L2, L5, L5, R2, L3, L4, R4, R4, R5, L4, L2, R4, L5, R3, R1, L1, R3, L2, R2, R1, R5, L4, R5, L3, R2, R3, R1, R4, L4, R1, R3, L5, L1, L3, R2, R1, R4, L4, R3, L3, R3, R2, L3, L3, R4, L2, R4, L3, L4, R5, R1, L1, R5, R3, R1, R3, R4, L1, R4, R3, R1, L5, L5, L4, R4, R3, L2, R1, R5, L3, R4, R5, L4, L5, R2";
-
-        return input.split(", ");
     }
 
 }
--- src/day1/Position.kt
+++ src/day1/Position.kt
@@ -2,6 +2,14 @@ package day1
 
 class Position(var direction: String, var x: Int, var y: Int) {
 
+    companion object {
+        var vistis: MutableList<Pair<Int, Int>> = ArrayList<Pair<Int, Int>>();
+
+        fun contains(cord: Pair<Int, Int>): Boolean{
+            return vistis.contains(cord);
+        }
+    }
+
     fun getDistance(): Int{
         return Math.abs(x) + Math.abs(y);
     }
