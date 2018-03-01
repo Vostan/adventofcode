1d5195c079a1a954bd8937ad054caa842345ea23
vostan <vostan.92@gmail.com>
2018-03-01 19:48:54 +0100

    day 6 part one and two done

---
+++ src/day6/Day6.kt
@@ -0,0 +1,38 @@
+package day6
+
+import java.io.File
+
+class Day6 {
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            Regex("^(.{1})", RegexOption.MULTILINE).findAll(getInput()).toList().map { it.groupValues[0] };
+
+            val reversedMatrix: ArrayList<ArrayList<String>> = arrayListOf();
+
+            getInput().split("\n")
+                    .map { it.split("").filter {it != ""} }.toList()
+                    .forEach{
+                        it.forEachIndexed { i,v ->
+                            if(reversedMatrix.size <= i){
+                                reversedMatrix.add(arrayListOf(v));
+                            } else{
+                                reversedMatrix[i].add(v);
+                            }
+                        }
+                    };
+
+            reversedMatrix.map { it.groupBy { it }.minBy { it.value.size }?.key }.forEach{print(it)};
+
+        }
+
+        private fun getInput(): String {
+            return File("./src/day6/input.txt").readText();
+        }
+
+
+
+    }
+}
